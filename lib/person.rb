27079ff721b7b775e0165bc052ec4a7cd676c6a1
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-19 20:52:22 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
 
+  
+  # Setter method
+  def name=(new_name)
+    @name = new_name
+  end
+
+  # Getter method
+  def name
+    @name
+  end
+
+  
+  # Setter method
+  def job=(job)
+    @job = job
+  end
+
+  # Getter method
+  def job
+    @job
+  end
+
+  
 end
